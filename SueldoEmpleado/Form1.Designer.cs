04619fe05db682e03b60af614b7fab9794f833ec
RealFURAI <164509106+Furai1@users.noreply.github.com>
2024-11-08 20:34:25 -0600

    Add files via upload

---
+++ SueldoEmpleado/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace SueldoEmpleado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.grpDatos = new System.Windows.Forms.GroupBox();
+            this.cmbCargo = new System.Windows.Forms.ComboBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.lblCargo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.grpClick = new System.Windows.Forms.GroupBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.lblSueldo = new System.Windows.Forms.Label();
+            this.txtSueldoCorrespondiente = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.grpDatos.SuspendLayout();
+            this.grpClick.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(335, 37);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(455, 32);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Determinar sueldo del empleado";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(530, 317);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 196);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // grpDatos
+            // 
+            this.grpDatos.Controls.Add(this.cmbCargo);
+            this.grpDatos.Controls.Add(this.txtnombre);
+            this.grpDatos.Controls.Add(this.lblCargo);
+            this.grpDatos.Controls.Add(this.lblNombre);
+            this.grpDatos.Location = new System.Drawing.Point(102, 116);
+            this.grpDatos.Name = "grpDatos";
+            this.grpDatos.Size = new System.Drawing.Size(548, 146);
+            this.grpDatos.TabIndex = 2;
+            this.grpDatos.TabStop = false;
+            this.grpDatos.Text = "Ingrese datos";
+            // 
+            // cmbCargo
+            // 
+            this.cmbCargo.FormattingEnabled = true;
+            this.cmbCargo.Items.AddRange(new object[] {
+            "Contador",
+            "Secretaria",
+            "Administrador",
+            "Gerente"});
+            this.cmbCargo.Location = new System.Drawing.Point(210, 93);
+            this.cmbCargo.Name = "cmbCargo";
+            this.cmbCargo.Size = new System.Drawing.Size(281, 33);
+            this.cmbCargo.TabIndex = 3;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(210, 38);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(281, 30);
+            this.txtnombre.TabIndex = 2;
+            this.txtnombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtnombre_KeyPress);
+            // 
+            // lblCargo
+            // 
+            this.lblCargo.AutoSize = true;
+            this.lblCargo.Location = new System.Drawing.Point(80, 93);
+            this.lblCargo.Name = "lblCargo";
+            this.lblCargo.Size = new System.Drawing.Size(72, 25);
+            this.lblCargo.TabIndex = 1;
+            this.lblCargo.Text = "Cargo:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(80, 38);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(87, 25);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // grpClick
+            // 
+            this.grpClick.Controls.Add(this.btnSalir);
+            this.grpClick.Controls.Add(this.btnCalcular);
+            this.grpClick.Controls.Add(this.btnNuevo);
+            this.grpClick.Location = new System.Drawing.Point(102, 293);
+            this.grpClick.Name = "grpClick";
+            this.grpClick.Size = new System.Drawing.Size(317, 252);
+            this.grpClick.TabIndex = 3;
+            this.grpClick.TabStop = false;
+            this.grpClick.Text = "Hacer Click";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(88, 163);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(141, 40);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(88, 49);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(141, 40);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(88, 106);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(141, 40);
+            this.btnNuevo.TabIndex = 0;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // lblSueldo
+            // 
+            this.lblSueldo.AutoSize = true;
+            this.lblSueldo.Location = new System.Drawing.Point(97, 573);
+            this.lblSueldo.Name = "lblSueldo";
+            this.lblSueldo.Size = new System.Drawing.Size(225, 25);
+            this.lblSueldo.TabIndex = 4;
+            this.lblSueldo.Text = "Sueldo correspondiente:";
+            // 
+            // txtSueldoCorrespondiente
+            // 
+            this.txtSueldoCorrespondiente.Location = new System.Drawing.Point(341, 573);
+            this.txtSueldoCorrespondiente.Name = "txtSueldoCorrespondiente";
+            this.txtSueldoCorrespondiente.Size = new System.Drawing.Size(281, 30);
+            this.txtSueldoCorrespondiente.TabIndex = 4;
+            this.txtSueldoCorrespondiente.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 652);
+            this.Controls.Add(this.txtSueldoCorrespondiente);
+            this.Controls.Add(this.lblSueldo);
+            this.Controls.Add(this.grpClick);
+            this.Controls.Add(this.grpDatos);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTitulo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Sueldo Empleados";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.grpDatos.ResumeLayout(false);
+            this.grpDatos.PerformLayout();
+            this.grpClick.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox grpDatos;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label lblCargo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.GroupBox grpClick;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Label lblSueldo;
+        private System.Windows.Forms.TextBox txtSueldoCorrespondiente;
+        private System.Windows.Forms.ComboBox cmbCargo;
+    }
+}
+
---
+++ SueldoEmpleado/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SueldoEmpleado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtSueldoCorrespondiente.ReadOnly = true;
+            txtSueldoCorrespondiente.TabStop = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            string cargo;
+            cargo = cmbCargo.Text;
+            if (cargo == "Gerente")
+            {
+                txtSueldoCorrespondiente.Text = "32,500";
+            }
+            if (cargo == "Administrador")
+            {
+                txtSueldoCorrespondiente.Text = "10,000";
+            }
+            if (cargo == "Secretaria")
+            {
+                txtSueldoCorrespondiente.Text = "5,500";
+            }
+            if (cargo == "Contador")
+            {
+                txtSueldoCorrespondiente.Text = "6,000";
+            }
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void txtnombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ( !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) &&
+                e.KeyChar != ' ')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            txtnombre.Text = "";
+            txtSueldoCorrespondiente.Text = "";
+            cmbCargo.Text = "";
+        }
+    }
+}
